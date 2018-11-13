cb4189498bb12a33ed4fded5d1cc0985242e27a2
Sabino Velasquez <sabinovelasquez@gmail.com>
2018-11-12 21:02:57 -0300

    readme wip, installer

--- main.js
+++ main.js
@@ -14,3 +14,4 @@ app.on('ready', () => {
     win = null
   })
 })
+// electron-packager . xbox_fullscreen --overwrite --asar=true --platform=win32 --arch=ia32 --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="Xbox Fullscreen"
